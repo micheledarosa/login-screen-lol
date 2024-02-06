b7ad0ace6eb32c9c86db48ec82eb1a1fb49aac6c
Michele da Rosa <misha.atelie@gmail.com>
2024-02-06 10:28:12 -0300

    music and show pass add

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const inputs = document.querySelectorAll('.input');
 const button = document.querySelector('.login__button');
+const togglePass = document.getElementById('togglePassword');
+const passwordInput = document.querySelector('input[name="password"]');
 
-const handleFocus = ({
-    target
-}) => {
+const handleFocus = ({ target }) => {
     const span = target.previousElementSibling;
     span.classList.add('span-active');
 }
 
-const handleFocusOut = ({
-    target
-}) => {
+const handleFocusOut = ({ target }) => {
     if (target.value === '') {
         const span = target.previousElementSibling;
         span.classList.remove('span-active')
@@ -27,9 +25,16 @@ const handleChange = () => {
     }
 }
 
+const handleTogglePassword = () => {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    togglePass.src = type === 'password' ? './images/eye-slash.png' : './images/eye.png';
+}
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
 
 inputs.forEach((input) => input.addEventListener('input', handleChange));
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
+
+togglePass.addEventListener('click', handleTogglePassword);
